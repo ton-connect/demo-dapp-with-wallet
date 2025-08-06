d45ceb22b771ec4e9ee820b883728d4edd1be5f5
Yauheni Mileika <vip.carich@gmail.com>
2025-08-06 11:11:52 +0300

    feat: add qaMode support

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { render } from 'react-dom';
 import App from './App'
 import './index.scss'
 import eruda from "eruda";
+import { enableQaMode } from '@tonconnect/ui-react';
+
+if (import.meta.env.VITE_QA_MODE === 'enable') {
+    enableQaMode();
+}
 
 eruda.init();
 
